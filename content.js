562de275296a8dbf688baf1185329160cd20eaeb
Nippers <juliauhr@gmail.com>
2017-07-29 13:48:31 -0600

    pen layout, content update

--- content.js
+++ content.js
@@ -1,21 +1,20 @@
-var dialog = [];
-dialog.push("");
-dialog.push("Only junk mail, per usual.");
-dialog.push("If we're in a computer game then someone must have created the game.");
-//"Like aliens or God or future humans?"
-//"Yep. So maybe it's the game developer who's been sending you letters."
-//"Why would the Developer write to me? If she created the game, she already knows and controls everything that's going to happen. It's impossible for me to surprise her."
-//"Who knows why the Developer does what she does. I wouldn't worry about it too much."
-//"I can't help worrying. I might not even be a real person!"
-var letters = [];
-letters.push("");
-letters.push("Dear Mr. Jiblet,<br>Who are you?<br>What is the world?");
 
-var canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext("2d"),
-    width = 600,
-    height = 330,
-    player = {
+var width = 600;
+var height = 330;
+var canvas = document.getElementById("canvas");
+
+canvas.width = width*2;
+canvas.height = height*2;
+
+canvas.style.width = width;
+canvas.style.height = height;
+
+ctx = canvas.getContext("2d");
+
+canvas.getContext("2d").scale(2,2);
+
+
+    var player = {
         x: width/2,
         y: -70,
         width: 14,
@@ -35,12 +34,12 @@ var canvas = document.getElementById("canvas"),
 var mailbox=({name: "mailbox", x:-80, y:270, width:35, height:50, open:false, flag:true, imgOffset:0});
 var dancer=({name:"dancer", x:750, y:80, width:45, height:150});
 var archer=({name:"archer", x:650, y:185, width:60, height:135});
-var fountain=({name:"fountain", x:970, y:25, width:85, height:115, imgOffset:0});
-var libraryDoor=({name:"libraryDoor", x:1115, y:10, width:70, height:180, open:true});
+var fountain=({name:"fountain", x:970, y:25, width:85, height:115, imgOffset:0, frames:3, animSpeed:5});
+var libraryDoor=({name:"libraryDoor", x:1115, y:10, width:70, height:180, imgOffset:0, open:true});
 var pinkGirl=({name:"pinkGirl", x:-450, y:-52, width:15, height:37, imgOffset:0});
 var yellowGirl=({name:"yellowGirl", x:-150, y:-40, width:29, height:25, imgOffset:0});
 var ball=({name:"ball", x:-545, y:-25, width:10, height:10, frames:4, imgOffset:0, goingRight:true, counter:0});
-var tree=({name:"tree", x:-589, y:-365, width:175, height:350, frames:2, imgOffset:0});
+var tree=({name:"tree", x:-589, y:-365, width:174, height:350, frames:2, imgOffset:0});
 var building=({name:"building", x:-width+11, y:5, width:400, height:315});
 var house=({name:"house", x:-width+210, y:-565, width:133, height:150});
 var city=({name:"city", x:0, y:130, width:600, height:200});
@@ -48,6 +47,9 @@ var library=({name:"library", x:1199, y:-300, width:800, height:500});
 var ladder=({name:"ladder", x:1900, y:6, width:45, height:173});
 var busts=({name:"busts", x:1650, y:110, width:590, height:180});
 var globe=({name:"globe", x:1240, y:-116, width:105, height:118, imgOffset:0});
+var windmill=({name:"windmill", x:30, y:-370, width:190, height:330, imgOffset:0, frames:4, animSpeed:5});
+var windmill2=({name:"windmill", x:330, y:-370, width:190, height:330, imgOffset:0, frames:4, animSpeed:5});
+
 
 var boxes = [];
 
@@ -68,6 +70,8 @@ boxes.push({x:0, y:120, width:30, height:30, skin:"square"});
 boxes.push({x:70, y:170, width:80, height:80, skin:"square"});
 boxes.push({x:160, y:220, width:20, height:20, skin:"square"});
 boxes.push({x:230, y:270, width:40, height:40, skin:"square"});
+//windfarm pllatform
+boxes.push({x:30, y:-40, width:500, height:20, skin:"rect"});
 //right garden
 boxes.push({x:700, y:280, width:500, height:40, skin:"rect"});
 boxes.push({x:750, y:230, width:450, height:50, skin:"rect"});
@@ -95,7 +99,7 @@ var skyTop = 0;
 var skyBottom = height;
 
 var movingObjects = []; //for moving the viewport
-movingObjects.push(globe,busts,library,ladder,tree,ball,mailbox,dancer,archer,fountain,libraryDoor,pinkGirl,building,house,yellowGirl,skyTop,skyBottom);
+movingObjects.push(windmill2,windmill,globe,busts,library,ladder,tree,ball,mailbox,dancer,archer,fountain,libraryDoor,pinkGirl,building,house,yellowGirl,skyTop,skyBottom);
 
 for(i=0;i<boxes.length;i++){
 	movingObjects.push(boxes[i]);
@@ -106,11 +110,11 @@ for(i=0;i<rungs.length;i++){
 
 var pics = [];
 //the order matters because some things are drawn on top of others
-pics.push(library,ladder,tree,dancer,archer,fountain,libraryDoor,pinkGirl,yellowGirl,player,globe,busts,ball,mailbox,building,house);
+pics.push(windmill2,windmill,library,ladder,tree,dancer,archer,fountain,libraryDoor,pinkGirl,yellowGirl,player,globe,busts,ball,mailbox,building,house);
 //foregroundPics.push(globe,busts,ball,mailbox,building,house);
+var animations = [];
+animations.push(fountain,windmill);
 
-canvas.width = width;
-canvas.height = height;
 
 //sound files
 /*
@@ -125,7 +129,8 @@ var letterCount = 0;
 var frameCount = 0;//loops from 0 to 1000
 var jibletOffsetY = 0;
 var jibletOffsetX = 0;
-var progressMarker = 0;
+progressMarker = 0;
+var stopSpeechTimer = 500;
 
 
 function update() {
@@ -209,6 +214,12 @@ for (i = 0; i < rungs.length; i++){
     if (dir === "l"){
       mailbox.open = true;
       say(1);
+      if (frameCount > 500){
+        stopSpeechTimer = frameCount-500;
+      }else{
+        stopSpeechTimer = frameCount+500;
+      }
+      openLetter(0);
     }else if(dir === "r"){
     	mailbox.open = false;
     }
@@ -242,17 +253,21 @@ for (i = 0; i < rungs.length; i++){
   if(mailbox.open === true){
   	mailbox.imgOffset = 35;
   } else {
-  	mailboximgOffset = 0;
+  	mailbox.imgOffset = 0;
   }
 
   //fountain cycle
+  animate(fountain, 2, 5);
+  animate(windmill, 3, 5);
+  animate(windmill2, 3, 5);
+  /*
   if(frameCount%5===0){
-  	if(fountain.imgffset < fountain.width*2){
+  	if(fountain.imgOffset < fountain.width*2){
   		fountain.imgOffset += fountain.width;
     }else{
     	fountain.imgOffset=0;
     }
-  }
+  }*/
 
 //fountain sound
   /*dir = colCheck(player, fountain);
@@ -339,25 +354,35 @@ for (i = 0; i < rungs.length; i++){
     drawTwo(pics[i]);
   }
 
-  if(frameCount===500){
+  if(frameCount===stopSpeechTimer){
     say(0);
   }
 
 }//end main loop
 
+function animate(pic, num, speed){
+  //simple non-interactive animations
+  if(frameCount%speed===0){
+  	if(pic.imgOffset < pic.width*num){
+  		pic.imgOffset += pic.width;
+    }else{
+    	pic.imgOffset=0;
+    }
+  }
+}//end animate function
+
 function drawTwo(pic){
-  //var name = pic.toString();
-  //if(x+boxWidth>0 && x<600){
-  	//if(y+boxHeight>0 && y<330){
+  //draw things that overlap with the viewport
+  if(pic.x+pic.width>0 && pic.x<600){
+  	if(pic.y+pic.height>0 && pic.y<330){
+      if(typeof(pic.imgOffset)==="number"){
+        ctx.drawImage(document.getElementById(pic.name+"Pic"),pic.imgOffset,0,pic.width,pic.height,pic.x,pic.y,pic.width,pic.height);
+      }else{
     	ctx.drawImage(document.getElementById(pic.name+"Pic"),0,0,pic.width,pic.height,pic.x,pic.y,pic.width,pic.height);
-    //}
-  //}
-}
-
-function anim(pic, frames, speed, direction){
-  //draw non-interactive animated images
-
-}//end anim function
+      }
+    }
+  }
+}//end draw function
 
 function draw(image, xOffset, yOffset, width, height, x, y, boxWidth, boxHeight){
   //draw images on canvas
@@ -367,6 +392,8 @@ function draw(image, xOffset, yOffset, width, height, x, y, boxWidth, boxHeight)
     }
   }
 }//end draw function
+
+
 
 function colCheck(shapeA, shapeB, solid) {
     // get the vectors to check against
@@ -413,6 +440,18 @@ function say(num){
   document.getElementById("text").innerHTML = dialog[num];
 }
 
+function openLetter(num){
+  document.getElementById("letterText").innerHTML = letters[num];
+  document.getElementById("letters").style.display = "block";
+}
+
+function closeLetter(){
+  document.getElementById("letters").style.display = "none";
+  if(progressMarker===0){
+    say(2);
+  }
+}
+
 
 document.body.addEventListener("keydown", function (e) {
     keys[e.keyCode] = true;
@@ -429,3 +468,17 @@ window.addEventListener("load", function () {
 
 //setInterval(update(), 100);
 setInterval(function(){update();}, 16);
+
+var dialog = [];
+dialog.push("");
+dialog.push("A letter with no address or stamp.");
+dialog.push("Weird... I'm Mr. Jiblet, accountant from the city of Urr. The world didn't come from anywhere, it's always been there, right? Or maybe it came from the big bang?");
+dialog.push("If we're in a computer game then someone must have created the game.");
+//"Like aliens or God or future humans?"
+//"Yep. So maybe it's the game developer who's been sending you letters."
+//"Why would the Developer write to me? If she created the game, she already knows and controls everything that's going to happen. It's impossible for me to surprise her."
+//"Who knows why the Developer does what she does. I wouldn't worry about it too much."
+//"I can't help worrying. I might not even be a real person!"
+//"What the fugnugget is this?"
+var letters = [];
+letters.push("Dear Mr. Jiblet,<br><br>Who are you?<br>Where does the world come from?");
